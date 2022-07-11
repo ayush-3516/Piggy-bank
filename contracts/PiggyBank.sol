f9fd5195412aefda80ffd653c2fdd6b605137d43
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-07-11 23:23:07 +0530

    Update PiggyBank.sol

--- contracts/PiggyBank.sol
+++ contracts/PiggyBank.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.10;
 
 contract PiggyBank {
+    event Deposit(uint256 amount);
     address public owner = msg.sender;
 
-    receive() external payable {}
+    receive() external payable {
+        emit Deposit(msg.value);
+    }
 
     function withdraw() external {
         require(msg.sender == owner, "not owner");
+        emit Withdraw(address(this).balance);
         selfdestruct(payable(msg.sender));
     }
 }
